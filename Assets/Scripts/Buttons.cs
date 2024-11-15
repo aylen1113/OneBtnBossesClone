b7a4fda59486afdf291bd6d8e54c6f72d17f3767
aylen1113 <aylenrociolopez31@gmail.com>
2024-11-15 16:29:40 -0300

    -

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -15,7 +15,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
 
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("LevelSelection");
         Time.timeScale = 1f;
     }
 
